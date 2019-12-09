6b15ef6a4d239c18614901c488d6f15ae9bd6174
prithvi4 <prithvi4@illinois.edu>
2019-12-09 17:13:34 -0600

    Added more questions.

--- app/src/main/java/com/example/triviagame/TriviaQuestion.java
+++ app/src/main/java/com/example/triviagame/TriviaQuestion.java
@@ -7,7 +7,14 @@ public class TriviaQuestion {
             "Is UIUC the oldest public school in Illinois?",
             "What was the first high level programming language?",
             "What syntax do you use when implementing a parent class?",
-            "Which sorting algorithm is the fastest?"
+            "Which sorting algorithm is the fastest?",
+            "Which of the following statements is true?",
+            "Which way can algorithms be implements?",
+            "What is the characteristic of a good function?",
+            "In Java a non-void function always has a ",
+            "What is a Java variable that refers to an object called?",
+            "Which of the following are Java primitive types",
+            "Which two things do objects combine?"
     };
 
 private String mChoices [][] = {
@@ -16,9 +23,24 @@ private String mChoices [][] = {
         {"Yes", "No", "It is a private school"},
         {"Short Code", "C++", "Autocode"},
         {"Implements", "Connects", "Extends"},
-        {"Insertion Sort", "Quick Sort", "Bubble Sort"}
+        {"Insertion Sort", "Quick Sort", "Bubble Sort"},
+        {"The size cannot be changed once the array is initialized",
+                "Arrays have a .length() property that is used to get their size",
+                "Values cannot be assigned to an array when it is initialized"},
+        {"Making computers do things over and over again fast",
+                "Making computers perform complex calculations",
+                "None of the above"},
+        {"Cannot be reused", "Can be easily tested", "Does things well sometimes"},
+        {"A list of arguments", "A return type", "All of the above"},
+        {"Reference variable", "Object variable", "Referring variable" },
+        {"Integer", "Character", "None of the above"},
+        {"State and behavior", "State and attitude", "None of the above"}
         };
-private String mCorrectAnswers[] = {"CS 427", "Challen", "No", "Short Code", "Extends", "Quick Sort"};
+private String mCorrectAnswers[] = {"CS 427", "Challen", "No", "Short Code", "Extends", "Quick Sort"
+        , "Arrays have a .length() property that is used to get their size",
+        "Making computers do things over and over again fast", "A return type", "Reference variable"
+        , "Character", "State and behavior"};
+
 public String getQuestion(int a) {
         String question = mQuestions[a];
         return question;
@@ -40,4 +62,4 @@ public String getCorrectAnswer(int a) {
         String answer = mCorrectAnswers[a];
         return answer;
         }
-        }
+}
